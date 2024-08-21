cc4d95b137eebfe80b4a64639ea972f578767bb2
MaryMakesThings <robbyroo1415@outlook.com>
2024-08-20 18:36:04 -0700

    Update jscript.js

--- jscript.js
+++ jscript.js
@@ -3,9 +3,11 @@ let H = window.innerHeight
 let W = window.innerWidth
 
 if ( W == 600 ) {
-    document.getElementsByClassName( "basicclass1" ).style.maxwidth = "600";
+    document.getElementsByClassName( "basicclass1" ).style.marginleft = "40";
+    document.getElementsByClassName( "basicclass1" ).style.marginright = "40";
 }
 
 if ( W < 600 ) {
-    document.getElementsByClassName( "basicclass1").style.maxwidth = "600";
+    document.getElementsByClassName( "basicclass1").style.marginright = "40";
+    document.getElementsByClassName( "basicclass1").style.marginleft = "40";
 }
