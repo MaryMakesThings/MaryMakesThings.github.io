8a2a83f5817ab57fdfa8cc9c9f0b7b7d666900ee
MaryMakesThings <robbyroo1415@outlook.com>
2024-08-20 18:31:51 -0700

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,11 @@
+let H = window.innerHeight
+
+let W = window.innerWidth
+
+if ( W == 600 ) {
+    document.getElementsByClassName( "basicclass1" ).style.maxwidth = "600";
+}
+
+if ( W < 600 ) {
+    document.getElementsByClassName( "basicclass1").style.maxwidth = "600";
+}
